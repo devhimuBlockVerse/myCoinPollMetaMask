80f78547fbf4e1cbc163b9f1ff498333e61cb10d
devHimuBlockVerse <himu.dev@blockversesolutions.com>
2025-05-03 21:01:41 +0600

    Feature Screen Completed

--- lib/application/presentation/screens/features/features_screen.dart
+++ lib/application/presentation/screens/features/features_screen.dart
@@ -1,22 +1,153 @@
 import 'package:flutter/material.dart';
 
+import '../../../../framework/components/featureCard.dart';
+
 class FeaturesScreen extends StatefulWidget {
   const FeaturesScreen({super.key});
 
   @override
-  State<FeaturesScreen> createState() => _FeaturesScreenState();
+  State<FeaturesScreen> createState() =>
+      _FeaturesScreenState();
 }
 
-class _FeaturesScreenState extends State<FeaturesScreen> {
+class _FeaturesScreenState
+    extends State<FeaturesScreen> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
+        extendBodyBehindAppBar: true,
+        backgroundColor: Color(0xFF0B0A1E),
       appBar: AppBar(
-        title: const Text('Features'),
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          'Features',
+          style: TextStyle(
+              fontFamily: 'Poppins',
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+              fontSize: 20),
+          textAlign: TextAlign.center,
+        ),
         centerTitle: true,
       ),
-      body: SafeArea(child: Center(child: Text('Features Screen', style: TextStyle(color: Colors.white),))),
+      body:  Stack(
+        children: [
+          Positioned(
+            top: -screenHeight * 0.01,
+            right: -screenWidth * 0.09,
+            child: Container(
+              width: screenWidth ,
+              height: screenWidth ,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+              ),
+              child: Image.asset(
+                'assets/icons/gradientBgImage.png',
+                width: screenWidth ,
+                height: screenHeight ,
+                fit: BoxFit.fill,
+              ),
+            ),
+
+          ),
+
+          SafeArea(
+            child:Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: screenWidth * 0.04,
+                vertical: screenHeight * 0.02,
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                
+                    FeatureCard(
+                      iconPath: 'assets/icons/cryptoLaunchpad.png',
+                      title: 'Crypto Launchpad',
+                      description: 'Discover and support new cryptocurrency projects with our secure, innovative, and user-friendly platform.',
+                      isSvg: false,
+                      onTap: () {
+                        print('Card clicked!');
+                        // Navigate or do any action here
+                      },
+                    ),
+                
+                
+                    SizedBox(height: screenHeight * 0.01),
+                    FeatureCard(
+                      iconPath: 'assets/icons/ecmToken.png',
+                      title: 'ECM Token',
+                      description: 'Powering the ecosystem, ECM Token enhances the experience, offering exclusive access to investments.',
+                      isSvg: false,
+                      onTap: () {
+                        print('Card clicked!');
+                        // Navigate or do any action here
+                      },
+                    ),
+
+                    SizedBox(height: screenHeight * 0.01),
+                    FeatureCard(
+                      iconPath: 'assets/icons/communityVoting.png',
+                      title: 'Community Voting',
+                      description: 'Our platform integrates community voting to allow users to participate in deciding which projects receive funding.',
+                      isSvg: false,
+                      onTap: () {
+                        print('Card clicked!');
+                        // Navigate or do any action here
+                      },
+                    ),
+                    SizedBox(height: screenHeight * 0.01),
+                    FeatureCard(
+                      iconPath: 'assets/icons/educationalRes.png',
+                      title: 'Educational Resources',
+                      description: 'Learn about blockchain, crypto, and launchpads with our comprehensive educational content.',
+                      isSvg: false,
+                      onTap: () {
+                        print('Card clicked!');
+                        // Navigate or do any action here
+                      },
+                    ),
+                    SizedBox(height: screenHeight * 0.01),
+                    FeatureCard(
+                      iconPath: 'assets/icons/metaVerseIntegration.png',
+                      title: 'Metaverse Integration',
+                      description: 'Access Androverse, our innovative metaverse, where users can buy virtual land and enjoy exclusive experiences with ECM Tokens.',
+                      isSvg: false,
+                      onTap: () {
+                        print('Card clicked!');
+                        // Navigate or do any action here
+                      },
+                    ),
+                    SizedBox(height: screenHeight * 0.01),
+                    FeatureCard(
+                      iconPath: 'assets/icons/projectLaunch.png',
+                      title: 'Project Launches',
+                      description: 'Gain early access to exclusive crypto launches and opportunities for ECM Token holders and MyCoinPoll community members.',
+                      isSvg: false,
+                      onTap: () {
+                        print('Card clicked!');
+                        // Navigate or do any action here
+                      },
+                    ),
+
+
+                  ],
+                ),
+              ),
+            )
+          )
+
+        ],
+      )
     );
   }
+
+
+
 }
+
+
 
---
+++ lib/framework/components/featureCard.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class FeatureCard extends StatelessWidget {
+  final String iconPath;
+  final String title;
+  final String description;
+  final bool isSvg;
+  final VoidCallback? onTap;
+
+  const FeatureCard({
+    Key? key,
+    required this.iconPath,
+    required this.title,
+    required this.description,
+    this.isSvg = false,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    // Responsive sizes
+    double iconWidth = screenWidth * 0.15;
+    double titleFontSize = screenWidth * 0.035;
+    double descFontSize = screenWidth * 0.025;
+
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: screenHeight * 0.015,
+          horizontal: screenWidth * 0.03,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: Color(0x33FFFFFF),
+            width: 1,
+          ),
+          gradient: LinearGradient(
+            colors: [
+              Color(0x22FFFFFF),
+              Colors.transparent,
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            stops: [0.0, 0.09],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Color(0x33000000),
+              blurRadius: 2,
+              offset: Offset(0, 4),
+              spreadRadius: 1,
+            ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+             isSvg
+                ? SvgPicture.asset(
+              iconPath,
+              width: iconWidth,
+              fit: BoxFit.contain,
+            )
+                : Image.asset(
+              iconPath,
+              width: iconWidth,
+              fit: BoxFit.contain,
+            ),
+            SizedBox(width: screenWidth * 0.01),
+            // Text
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min, // Wrap height
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: titleFontSize,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                      height: 1.2, // Less vertical gap
+                    ),
+                  ),
+                  SizedBox(height: screenHeight * 0.01),
+                  Opacity(
+                    opacity: 0.7,
+                    child: Text(
+                      description,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: descFontSize,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w400,
+                        height: 1.2,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

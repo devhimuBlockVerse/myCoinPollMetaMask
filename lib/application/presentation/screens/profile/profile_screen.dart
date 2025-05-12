e67226c045aa95360776e46755c8b950acb437b8
Rashadul-hub <rashadulislamhimu@gmail.com>
2025-05-12 23:50:29 +0600

    ongoing Profile Screen, Widget Structuring

--- lib/application/presentation/screens/profile/profile_screen.dart
+++ lib/application/presentation/screens/profile/profile_screen.dart
@@ -11,28 +11,116 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Learn & Build'),
-        centerTitle: true,
-      ),
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.transparent,
+
       body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+          top: false,
 
-
-
-
-              ProfileOptionContainer(
-                labelText: 'Tax Statements',
-                leadingIconPath: 'assets/icons/taxStatement.svg',
-                trailingIconPath: 'assets/icons/rightArrow.svg',
-                onTrailingIconTap: () {
-                  print("Trailing icon tapped");
-                },
+          child: Container(
+            width: screenWidth,
+            height: screenHeight,
+            decoration: BoxDecoration(
+              color: const Color(0xFF01090B),
+              image: DecorationImage(
+                image: AssetImage('assets/icons/starGradientBg.png'),
+                fit: BoxFit.cover,
+                alignment: Alignment.topRight,
               ),
-            ],
+            ),
+
+
+            child: Column(
+               children: [
+                 SizedBox(height: screenHeight * 0.02),
+                 Align(
+                   alignment: Alignment.topCenter,
+                   child:  Text(
+                     'Profile',
+                     style: TextStyle(
+                       fontFamily: 'Poppins',
+                       color: Colors.white,
+                       fontWeight: FontWeight.w600,
+                       // fontSize: 20
+                       fontSize: screenWidth * 0.05,
+                     ),
+                     textAlign: TextAlign.center,
+                   ),
+                 ),
+
+                 Expanded(
+                   child: Padding(
+                     padding: EdgeInsets.symmetric(
+                       horizontal: screenWidth * 0.04,
+                       vertical: screenHeight * 0.02,
+                     ),
+                     child: SingleChildScrollView(
+                       child: Column(
+                         crossAxisAlignment: CrossAxisAlignment.center,
+                         children: [
+                           SizedBox(height: screenHeight * 0.02),
+                           Container(
+                             width: screenWidth,
+                             decoration: BoxDecoration(
+                               color: const Color(0xFF01090B),
+                               image: DecorationImage(
+                                 // image: AssetImage('assets/icons/gradientBgImage.png'),
+                                 // fit: BoxFit.contain,
+                                 image: AssetImage('assets/icons/profileFrameBg.png'),
+                                 fit: BoxFit.contain,
+                                 alignment: Alignment.topRight,
+                               ),
+                             ),
+
+                             child: Column(
+                               children: [
+                                 SizedBox(height: screenHeight * 0.02),
+                                 SizedBox(height: screenHeight * 0.02),
+                                 SizedBox(height: screenHeight * 0.02),
+                                 SizedBox(height: screenHeight * 0.02),
+
+                                 ProfileOptionContainer(
+                                  labelText: 'Tax Statements',
+                                  leadingIconPath: 'assets/icons/taxStatement.svg',
+                                  trailingIconPath: 'assets/icons/rightArrow.svg',
+                                  onTrailingIconTap: () {
+                                    print("Trailing icon tapped");
+                                  },
+                                 ),
+                                 ProfileOptionContainer(
+                                  labelText: 'Tax Statements',
+                                  leadingIconPath: 'assets/icons/taxStatement.svg',
+                                  trailingIconPath: 'assets/icons/rightArrow.svg',
+                                  onTrailingIconTap: () {
+                                    print("Trailing icon tapped");
+                                  },
+                                 ),
+                                 ProfileOptionContainer(
+                                  labelText: 'Tax Statements',
+                                  leadingIconPath: 'assets/icons/taxStatement.svg',
+                                  trailingIconPath: 'assets/icons/rightArrow.svg',
+                                  onTrailingIconTap: () {
+                                    print("Trailing icon tapped");
+                                  },
+                                 ),
+                               ],
+                             ),
+                           ),
+
+
+
+
+                         ],
+                       ),
+                     ),
+                   ),
+                 ),
+              ],
+            ),
           )
 
       ),

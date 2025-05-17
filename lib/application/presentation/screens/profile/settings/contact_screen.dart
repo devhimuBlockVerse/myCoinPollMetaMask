7fe4401f689fc2c81acbd8505c8f869279831860
devHimuBlockVerse <himu.dev@blockversesolutions.com>
2025-05-17 17:48:57 +0600

    Ongoing Phone and Email Modal Functionality  for user Contact, Implemeted Schema Host for Tel and email communication for user , Update Application layer file with Quesries for Contact , Integrated Full Contact Feature Functionality by one click On Phone or Email

--- lib/application/presentation/screens/profile/settings/contact_screen.dart
+++ lib/application/presentation/screens/profile/settings/contact_screen.dart
@@ -1,7 +1,9 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+import '../../../../../framework/components/BlockButton.dart';
 import '../../../../../framework/components/ListingFields.dart';
 import '../../../../../framework/utils/dynamicFontSize.dart';
 
@@ -18,6 +20,7 @@ class _ContactScreenState extends State<ContactScreen> {
   TextEditingController fullNameController = TextEditingController();
   TextEditingController emailAddressController = TextEditingController();
   TextEditingController subjectController = TextEditingController();
+  TextEditingController messageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -79,7 +82,7 @@ class _ContactScreenState extends State<ContactScreen> {
                       ),
                     ),
                   ),
-                  SizedBox(width: screenWidth * 0.12), // Responsive spacer for balance
+                  SizedBox(width: screenWidth * 0.12),
                 ],
               ),
 
@@ -101,34 +104,96 @@ class _ContactScreenState extends State<ContactScreen> {
 
 
                           Container(
-                            width: MediaQuery.of(context).size.width, // Full width
+                            width: double.infinity,
                             padding: EdgeInsets.symmetric(
                               horizontal: MediaQuery.of(context).size.width * 0.05,
                               vertical: MediaQuery.of(context).size.height * 0.02,
                             ),
                             decoration: BoxDecoration(
-                              color: const Color(0xff040C16), // Background color
-                              borderRadius: BorderRadius.circular(12), // Optional rounded corners
+                              color: const Color(0xff040C16),
+                              borderRadius: BorderRadius.circular(12),
                             ),
                             child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
                               mainAxisSize: MainAxisSize.min, // Content height
                               children: [
 
                                 ListingField(
-                                  // controller: projectDetailsController,
+                                  controller: fullNameController,
                                   labelText: 'Full Name',
-                                  height: screenHeight * 0.09,
+                                  height: screenHeight * 0.049,
+                                  width: screenWidth* 0.88,
+                                  expandable: false,
+                                  keyboard: TextInputType.name,
+                                ),
+
+                                SizedBox(height: screenHeight * 0.02),
+
+                                ListingField(
+                                  controller: emailAddressController,
+                                  labelText: 'Email',
+                                  height: screenHeight * 0.049,
+                                  width: screenWidth* 0.88,
+                                  expandable: false,
+                                  keyboard: TextInputType.emailAddress,
+                                ),
+
+
+                                SizedBox(height: screenHeight * 0.02),
+
+                                ListingField(
+                                  controller: subjectController,
+                                  labelText: 'Subject',
+                                  height: screenHeight * 0.049,
+                                  width: screenWidth* 0.88,
+                                  expandable: false,
+                                  keyboard: TextInputType.multiline,
+                                ),
+
+                                SizedBox(height: screenHeight * 0.02),
+
+                                ListingField(
+                                  controller: messageController,
+                                  labelText: 'Message',
+                                  height: screenHeight * 0.3,
                                   width: screenWidth* 0.88,
                                   expandable: true,
                                   keyboard: TextInputType.multiline,
                                 ),
+
+                                SizedBox(height: screenHeight * 0.03),
+
+                                BlockButton(
+                                  height: screenHeight * 0.045,
+                                  width: screenWidth * 0.6,
+                                  label: 'Submit',
+                                  textStyle: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.white,
+                                    fontSize: baseSize * 0.044,
+                                  ),
+                                  gradientColors: const [
+                                    Color(0xFF2680EF),
+                                    Color(0xFF1CD494),
+                                  ],
+                                  onTap: () {
+
+                                  },
+                                ),
+
+                                SizedBox(height: screenHeight * 0.01),
+
+
 
                               ],
                             ),
                           ),
 
 
+                ContactInfoWidget(
+                            phoneNumber: '012 3456 789',
+                            emailAddress: 'info@mycoinpoll.com',
+                          ),
 
 
 
@@ -144,3 +209,260 @@ class _ContactScreenState extends State<ContactScreen> {
     );
   }
 }
+
+
+class ContactInfoWidget extends StatelessWidget {
+  final String phoneNumber;
+  final String emailAddress;
+
+  const ContactInfoWidget({
+    Key? key,
+    required this.phoneNumber,
+    required this.emailAddress,
+  }) : super(key: key);
+
+  // Function to launch phone dialer
+  Future<void> _callPhone(String phoneNumber) async {
+    final Uri launchUri = Uri(scheme: 'tel', path: phoneNumber);
+    if (await canLaunchUrl(launchUri)) {
+      await launchUrl(launchUri);
+    } else {
+      debugPrint('Could not launch $launchUri');
+    }
+  }
+
+  // Function to launch email client
+  Future<void> _sendEmail(String emailAddress) async {
+    final Uri launchUri = Uri(scheme: 'mailto', path: emailAddress);
+    if (await canLaunchUrl(launchUri)) {
+      await launchUrl(launchUri);
+    } else {
+      debugPrint('Could not launch $launchUri');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    final isTablet = screenWidth > 600;
+
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(
+        horizontal: screenWidth * 0.04,
+        vertical: screenHeight * 0.02,
+      ),
+      decoration: BoxDecoration(
+        color: const Color(0xFF101A29), // Dark background
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Flex(
+        direction: isTablet ? Axis.horizontal : Axis.vertical,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: InkWell(
+              onTap: () => _callPhone(phoneNumber),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(Icons.phone, color: Colors.tealAccent),
+                  SizedBox(width: screenWidth * 0.02),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'PHONE',
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: screenWidth * 0.03,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          phoneNumber,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: screenWidth * 0.04,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: isTablet ? 0 : screenHeight * 0.02, width: isTablet ? screenWidth * 0.04 : 0),
+          Expanded(
+            child: InkWell(
+              onTap: () => _sendEmail(emailAddress),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(Icons.email, color: Colors.tealAccent),
+                  SizedBox(width: screenWidth * 0.02),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'EMAIL',
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: screenWidth * 0.03,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          emailAddress,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: screenWidth * 0.04,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// class ContactInfoWidget extends StatefulWidget {
+//   final String phoneNumber;
+//   final String emailAddress;
+//
+//   const ContactInfoWidget({
+//     Key? key,
+//     required this.phoneNumber,
+//     required this.emailAddress,
+//   }) : super(key: key);
+//
+//   @override
+//   State<ContactInfoWidget> createState() => _ContactInfoWidgetState();
+// }
+//
+// class _ContactInfoWidgetState extends State<ContactInfoWidget> {
+//   // Function to launch phone dialer
+//   Future<void> _callPhone(String phoneNumber) async {
+//     final Uri launchUri = Uri(
+//       scheme: 'tel',
+//       path: phoneNumber,
+//     );
+//     if (await canLaunchUrl(launchUri)) {
+//       await launchUrl(launchUri);
+//     } else {
+//       // Handle error, e.g., show a Snackbar
+//       print('Could not launch $launchUri');
+//     }
+//   }
+//
+//   // Function to launch email client
+//   Future<void> _sendEmail(String emailAddress) async {
+//     final Uri launchUri = Uri(
+//       scheme: 'mailto',
+//       path: emailAddress,
+//     );
+//     if (await canLaunchUrl(launchUri)) {
+//       await launchUrl(launchUri);
+//     } else {
+//       // Handle error, e.g., show a Snackbar
+//       print('Could not launch $launchUri');
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final screenWidth = MediaQuery.of(context).size.width;
+//     final screenHeight = MediaQuery.of(context).size.height;
+//     return Container(
+//       width: screenWidth,
+//       padding: const EdgeInsets.all(16.0),
+//       decoration: BoxDecoration(
+//         color: Colors.grey[850], // Dark background color
+//         borderRadius: BorderRadius.circular(12.0),
+//       ),
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         children: [
+//           // Phone Section
+//           Expanded(
+//             child: InkWell(
+//               onTap: () => _callPhone(widget.phoneNumber),
+//               child: Row(
+//                 children: [
+//                   const Icon(Icons.phone, color: Colors.tealAccent), // Phone icon
+//                   const SizedBox(width: 8.0),
+//                   Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Text(
+//                         'PHONE',
+//                         style: TextStyle(
+//                           color: Colors.white70,
+//                           fontSize: 12.0,
+//                           fontWeight: FontWeight.bold,
+//                         ),
+//                       ),
+//                       Text(
+//                         widget.phoneNumber,
+//                         style: TextStyle(
+//                           color: Colors.white,
+//                           fontSize: 16.0,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//           const SizedBox(width: 16.0), // Space between sections
+//
+//           // Email Section
+//           Expanded(
+//             child: InkWell(
+//               onTap: () => _sendEmail(widget.emailAddress),
+//               child: Row(
+//                 children: [
+//                   const Icon(Icons.email, color: Colors.tealAccent), // Email icon
+//                   const SizedBox(width: 8.0),
+//                   Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Text(
+//                         'EMAIL',
+//                         style: TextStyle(
+//                           color: Colors.white70,
+//                           fontSize: 12.0,
+//                           fontWeight: FontWeight.bold,
+//                         ),
+//                       ),
+//                       Text(
+//                         widget.emailAddress,
+//                         style: TextStyle(
+//                           color: Colors.white,
+//                           fontSize: 16.0,
+//                         ),
+//                         overflow: TextOverflow.ellipsis, // Prevent overflow
+//                       ),
+//                     ],
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }

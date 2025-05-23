53b4455775c4fd28b752bcda222722db1fcef6de
Rashadul-hub <rashadulislamhimu@gmail.com>
2025-05-23 18:49:51 +0600

    Funlly Functional Data Sorting Functionality Added

--- lib/application/domain/usecases/sort_data.dart
+++ lib/application/domain/usecases/sort_data.dart
@@ -1,27 +1,6 @@
 import '../../../framework/utils/date_parser.dart';
 import '../../../framework/utils/enums/sort_option.dart';
 
-// void _sortData(SortOption option) {
-//   setState(() {
-//     _currentSort = option;
-//
-//     _filteredData.sort((a, b) {
-//       switch (option) {
-//         case SortOption.dateLatest:
-//           return parseDate(b['Date']!).compareTo(parseDate(a['Date']!));
-//         case SortOption.dateOldest:
-//           return parseDate(a['Date']!).compareTo(parseDate(b['Date']!));
-//         case SortOption.statusAsc:
-//           return (a['Status'] ?? '').compareTo(b['Status'] ?? '');
-//         case SortOption.statusDesc:
-//           return (b['Status'] ?? '').compareTo(a['Status'] ?? '');
-//       }
-//     });
-//   });
-// }
-
-
-
 
 class SortDataUseCase {
   List<Map<String, dynamic>> call(
--- lib/framework/utils/date_parser.dart
+++ lib/framework/utils/date_parser.dart
@@ -1,8 +1,5 @@
 import 'package:intl/intl.dart';
 
-// DateTime parseDate(String dateStr) {
-//   return DateFormat('MMMM d, yyyy').parse(dateStr);
-// }
 
 class DateParser {
   static DateTime fromReadableFormat(String dateStr) {

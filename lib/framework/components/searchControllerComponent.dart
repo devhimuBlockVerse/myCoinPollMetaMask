58b1f67914c6641bbb0f8b5e88aa9350c4a1e74c
devHimuBlockVerse <himu.dev@blockversesolutions.com>
2025-05-23 18:40:42 +0600

    Added Pop Up view model for data sorting functionality using  TimSort algorithm., maintain clean architure directory

---
+++ lib/framework/components/searchControllerComponent.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../utils/dynamicFontSize.dart';
+
+class ResponsiveSearchField extends StatelessWidget {
+  final TextEditingController controller;
+  final ValueChanged<String>? onChanged;
+  final String hintText;
+  final String? svgAssetPath;
+
+  const ResponsiveSearchField({
+    Key? key,
+    required this.controller,
+    this.onChanged,
+    this.hintText = 'search...',this.svgAssetPath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    double containerWidth = screenWidth * 0.55;
+    double containerHeight = screenHeight * 0.040;
+    double iconSize = screenHeight * 0.0135;
+    // double fontSize = screenHeight * 0.014;
+    double fontSize = getResponsiveFontSize(context, 13);
+
+    return Container(
+      width: containerWidth,
+      height: containerHeight,
+      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.02),
+      decoration: ShapeDecoration(
+        color: const Color(0xFF101A29),
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(width: 1, color: Color(0xFF141317)),
+          borderRadius: BorderRadius.circular(3),
+        ),
+        shadows: const [
+          BoxShadow(
+            color: Color(0xFFC7E0FF),
+            blurRadius: 0,
+            offset: Offset(0.10, 0.50),
+            spreadRadius: 0,
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          // SvgPicture.asset('assets/icons/search.svg',fit:BoxFit.contain,),
+
+          Opacity(
+            opacity: 0.80,
+            child: SvgPicture.asset(
+              svgAssetPath!,
+              width: iconSize,
+              height: iconSize,
+              colorFilter: const ColorFilter.mode(
+                Color(0xFF7D8FA9),
+                BlendMode.srcIn,
+              ),
+            ),
+          ),          SizedBox(width: screenWidth * 0.015),
+          Expanded(
+            child: TextField(
+              controller: controller,
+              onChanged: onChanged,
+              style: TextStyle(
+                color: const Color(0xFF7D8FA9),
+                fontSize: fontSize,
+                fontFamily: 'Poppins',
+              ),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                isCollapsed: true,
+                hintText: hintText,
+                hintStyle: TextStyle(
+                    color: const Color(0xFF7D8FA9).withOpacity(0.8),
+                    fontSize: fontSize,
+                    fontFamily: 'Poppins',
+                    height: 1.6
+                ),
+              ),
+              cursorColor: const Color(0xFF7D8FA9),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

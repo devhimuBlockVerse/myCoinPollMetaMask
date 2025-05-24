4f3fd102c2308fd98efe241f2b8fa7ac9652b9c3
Rashadul-hub <rashadulislamhimu@gmail.com>
2025-05-24 23:04:58 +0600

    Implemented new Fields for Update Passowrd in Personal Information Section

--- lib/application/presentation/screens/profile/personal_info/personal_information.dart
+++ lib/application/presentation/screens/profile/personal_info/personal_information.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:mycoinpoll_metamask/application/presentation/screens/profile/profile_screen.dart';
 import 'package:mycoinpoll_metamask/application/presentation/viewmodel/personal_information_viewmodel/personal_view_model.dart';
+import 'package:mycoinpoll_metamask/framework/utils/dynamicFontSize.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../../framework/components/BlockButton.dart';
 import '../../../../../framework/components/ListingFields.dart';
+import '../../../../../framework/components/customDropDownComponent.dart';
 
 
 class PersonalInformationScreen extends StatefulWidget {
@@ -286,7 +288,6 @@ class _PersonalInformationScreenState extends State<PersonalInformationScreen> {
 
                                    SizedBox(height: screenHeight * 0.03),
 
-
                                   BlockButton(
                                     height: screenHeight * 0.045,
                                     width: screenWidth * 0.7,
@@ -294,7 +295,8 @@ class _PersonalInformationScreenState extends State<PersonalInformationScreen> {
                                     textStyle: TextStyle(
                                       fontWeight: FontWeight.w700,
                                       color: Colors.white,
-                                      fontSize: baseSize * 0.030,
+                                      // fontSize: baseSize * 0.030,
+                                      fontSize: getResponsiveFontSize(context, 16),
                                     ),
                                     gradientColors: const [
                                       Color(0xFF2680EF),
@@ -401,96 +403,5 @@ class _PersonalInformationScreenState extends State<PersonalInformationScreen> {
 
 }
 
-class CustomDropdown extends StatelessWidget {
-  final String label;
-  final List<String> items;
-  final String? selectedValue;
-  final ValueChanged<String?> onChanged;
-  final double? width;  // Made nullable
-  final double? height; // Made nullable
-
-  const CustomDropdown({
-    super.key,
-    required this.label,
-    required this.items,
-    required this.selectedValue,
-    required this.onChanged,
-    this.width,
-    this.height,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
-    final double screenHeight = MediaQuery.of(context).size.height;
-
-    final double responsiveWidth = width ?? screenWidth * 0.4;
-    final double responsiveHeight = height ?? screenHeight * 0.040;
-
-    final double fontSize = screenHeight * 0.016;
-    final double horizontalPadding = screenWidth * 0.025;
-    final double borderRadius = screenWidth * 0.008;
-
-    return Container(
-      width: responsiveWidth,
-      height: responsiveHeight,
-      padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-      decoration: ShapeDecoration(
-        color: const Color(0xFF101A29),
-        shape: RoundedRectangleBorder(
-          side: const BorderSide(width: 1, color: Color(0xFF141317)),
-          borderRadius: BorderRadius.circular(borderRadius),
-        ),
-        shadows: const [
-          BoxShadow(
-            color: Color(0xFFC7E0FF),
-            blurRadius: 0,
-            offset: Offset(0.10, 0.50),
-            spreadRadius: 0,
-          ),
-        ],
-      ),
-      child: DropdownButtonHideUnderline(
-        child: DropdownButton<String>(
-          dropdownColor: const Color(0xFF101A29),
-          value: selectedValue,
-          isExpanded: true,
-          iconSize: screenHeight * 0.025,
-          icon: SvgPicture.asset(
-            'assets/icons/dropDownIcon.svg',
-            height: screenHeight * 0.01,
-            colorFilter: const ColorFilter.mode(Color(0xFF7D8FA9), BlendMode.srcIn),
-          ),
-          hint: Text(
-            label,
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.w400,
-              fontSize: fontSize,
-              height: 1.6,
-              color: Colors.white.withOpacity(0.4),
-            ),
-          ),
-          onChanged: onChanged,
-          items: items.map((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(
-                value,
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w400,
-                  fontSize: fontSize,
-                  height: 1.6,
-                  color: Colors.white.withOpacity(0.8),
-                ),
-              ),
-            );
-          }).toList(),
-        ),
-      ),
-    );
-  }
-}
 
 

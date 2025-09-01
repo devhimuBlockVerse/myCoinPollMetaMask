a162be4dc4253a8b781221eeac21cc30c11a5b72
APP-Himu <himu.dev@blockversesolutions.com>
2025-09-01 16:28:56 +0600

    News screen Model Updated

--- lib/application/presentation/models/news_model.dart
+++ lib/application/presentation/models/news_model.dart
@@ -1,3 +1,32 @@
+class ExternalLink {
+  final String pr;
+  final String date;
+  final String link;
+  final String slug;
+  final String photo;
+  final String title;
+
+  ExternalLink({
+    required this.pr,
+    required this.date,
+    required this.link,
+    required this.slug,
+    required this.photo,
+    required this.title,
+  });
+
+  factory ExternalLink.fromJson(Map<String, dynamic> json) {
+    return ExternalLink(
+      pr: json['pr'] ?? '',
+      date: json['date'] ?? '',
+      link: json['link'] ?? '',
+      slug: json['slug'] ?? '',
+      photo: json['photo'] ?? '',
+      title: json['title'] ?? '',
+    );
+  }
+}
+
 class NewsModel {
   final int id;
   final String title;
@@ -10,6 +39,7 @@ class NewsModel {
   final String shortDescription;
   final String description;
   final String createdAt;
+  final List<ExternalLink>? externalLinks;
 
   NewsModel({
     required this.id,
@@ -23,6 +53,7 @@ class NewsModel {
     required this.shortDescription,
     required this.description,
     required this.createdAt,
+    this.externalLinks,
   });
 
   factory NewsModel.fromJson(Map<String, dynamic> json) {
@@ -38,6 +69,43 @@ class NewsModel {
       shortDescription: json['short_description'],
       description: json['description'],
       createdAt: json['created_at'],
+      externalLinks: json['external_links'] != null
+          ? (json['external_links'] as List)
+          .map((link) => ExternalLink.fromJson(link))
+          .toList()
+          : null,
     );
   }
+
+  // Added method to convert back to JSON if needed
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'slug': slug,
+      'image': image,
+      'image_large': imageLarge,
+      'news_category_id': newsCategoryId,
+      'tags': tags,
+      'status': status,
+      'short_description': shortDescription,
+      'description': description,
+      'created_at': createdAt,
+      'external_links': externalLinks?.map((link) => link.toJson()).toList(),
+    };
+  }
 }
+
+extension ExternalLinkExtension on ExternalLink {
+  Map<String, dynamic> toJson() {
+    return {
+      'pr': pr,
+      'date': date,
+      'link': link,
+      'slug': slug,
+      'photo': photo,
+      'title': title,
+    };
+  }
+}
+
